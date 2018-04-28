4ed0029cd4a423d61e9d85d289d9cda2aaf4253e
lianglingbo <lianglingbo@joymeter.com>
2018-04-28 15:26:40 +0800

    增加离线率插入功能

--- src/main/java/com/joymeter/task/ScheduledForDynamicCron.java
+++ src/main/java/com/joymeter/task/ScheduledForDynamicCron.java
@@ -26,15 +26,18 @@ public class ScheduledForDynamicCron implements SchedulingConfigurer {
     private static final String analysisRate = PropertiesUtils.getProperty("analysisRate", "1");
     private static final Logger logger = Logger.getLogger(ScheduledForDynamicCron.class.getName());
     private static final String queryUrl = PropertiesUtils.getProperty("queryUrl","");
-    private static final String postUrl = PropertiesUtils.getProperty("postUrl","");
+    private static final String postEnUrl = PropertiesUtils.getProperty("postEnUrl","");
+    private static final String postOfUrl = PropertiesUtils.getProperty("postOfUrl","");
     private static final String[] types = PropertiesUtils.getProperty("types","").split("\\.");
     private static final String QUERY_TYPE_DATA =PropertiesUtils.getProperty("QUERY_TYPE_DATA","");
     private static final String QUERY_OFFLINE_DEVICEID = PropertiesUtils.getProperty("QUERY_OFFLINE_DEVICEID","");
     private static final String QUERY_DEVICEID_STATUS = PropertiesUtils.getProperty("QUERY_DEVICEID_STATUS","");
-
+    private static final String QUERY_TOTAL_DEVICEID = PropertiesUtils.getProperty("QUERY_TOTAL_DEVICEID","");
+    
     @Override
     public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
-        if (StringUtils.isEmpty(queryUrl) || StringUtils.isEmpty(postUrl)
+        if (StringUtils.isEmpty(queryUrl) || StringUtils.isEmpty(postEnUrl)
+        		|| StringUtils.isEmpty(postOfUrl)
                 || (types==null || (types!=null && types.length==0))) return;
         try {
             taskRegistrar.addTriggerTask(() -> {
@@ -57,15 +60,33 @@ public class ScheduledForDynamicCron implements SchedulingConfigurer {
             try {
                 String result = HttpClient.sendPost(queryUrl, String.format(QUERY_TYPE_DATA, type));
                 String data = result.contains("sumdata")?result.substring(result.indexOf(":")+1,result.indexOf("}")):"0";
-                HttpClient.sendPost(postUrl, "{\"type\":\"" + type + "\",\"data\":\"" + data + "\",\"datetime\":\"" + System.currentTimeMillis() + "\"}");
+                HttpClient.sendPost(postEnUrl, "{\"type\":\"" + type + "\",\"data\":\"" + data + "\",\"datetime\":\"" + System.currentTimeMillis() + "\"}");
             }catch (Exception e) {
                 logger.log(Level.SEVERE,null,e);
             }
         }
     }
+    /**
+     * 计算离线率，存入Druid
+     * 1.查询设备总数量
+     */
+    public void setOfflineRate() {
+    	 try {
+             String result = HttpClient.sendPost(queryUrl, String.format(QUERY_TOTAL_DEVICEID));
+             String totalNum = result.contains("totalNum")?result.substring(result.indexOf(":")+1,result.indexOf("}")):"0";
+             int offNum = getOfflineCount();
+             //设备类型暂时传空
+             HttpClient.sendPost(postOfUrl, "{\"type\":\"" + ' ' + "\",\"offNum\":\"" + offNum + "\",\"totalNum\":\"" + totalNum +"\",\"datetime\":\"" + System.currentTimeMillis() + "\"}");
+         }catch (Exception e) {
+             logger.log(Level.SEVERE,null,e);
+         }
+    }
     
     /**
      * 计算离线设备数量
+     * 1.按设备id分组，查询事件为‘离线’，的所有设备id
+     * 2.遍历此结果，得到每个设备id
+     * 3.通过设备id去查询此设备最新的状态，统计最新状态为离线的设备数量
      * @return
      */
     public int getOfflineCount() {
