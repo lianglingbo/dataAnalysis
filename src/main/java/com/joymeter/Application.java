68c7259e986b604d6af62f6d89f17c404658dbd2
panAstin <1104660383@qq.com>
2018-08-01 15:41:54 +0800

    移除无用引入

--- src/main/java/com/joymeter/Application.java
+++ src/main/java/com/joymeter/Application.java
@@ -6,11 +6,9 @@ import java.util.logging.Logger;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import com.joymeter.task.Consumer;
 import com.joymeter.task.Scheduler;
 import com.joymeter.util.ThreadsExecutor;
 import com.joymeter.util.common.SpringBean;
